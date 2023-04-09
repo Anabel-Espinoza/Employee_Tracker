45f48783204f396ac330dfc1676cbc0ffa5d3439
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-04-09 15:37:36 -0400

    Added file structure. Added db schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS management_db;
+CREATE DATABASE management_db;
+
+USE management_db;
+
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL -- double check this
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id) 
+    ON DELETE SET NULL -- null if the employee has no manager
+)
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Sales"),
+        ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Sales Lead", 100000, 3),
+        ("Salesperson", 80000, 3),
+        ("Lead Engineer", 150000, 1),
+        ("Software Engineer", 120000, 1),
+        ("Account Manager", 160000, 2),
+        ("Accountant", 120000, 2),
+        ("Legal Team Leader", 250000, 4),
+        ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("John", "Smoltz", 1),
+        ("Greg", "Maddux", 2, 1),
+        ("Tom", "Glavine", 3),
+        ("Rafa", "Nadal", 4, 3),
+        ("Sergio", "Perez", 5),
+        ("Carlos", "Alcaraz", 6, 5),
+        ("Mary", "Fried", 7),
+        ("Laura", "Freeman", 8, 7);
