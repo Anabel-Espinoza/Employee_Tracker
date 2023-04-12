631f4b578caaf75dd1b94ba5c48852f380a36d88
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-04-11 20:17:32 -0400

    Fixed the display init question. Added add department and add role functions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require ('inquirer')
 const mysql = require('mysql2')
-const tables = require('console.table')
+const cTable = require('console.table')
 let arrayDepartments = []
+let arrayRoles = []
+let arrayEmployees = []
 
 const db = mysql.createConnection(
     {
@@ -12,9 +14,6 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the management_db database')
 )
-
-const viewRoles= 'SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON  role.department_id = department.id ORDER BY role.id'
-const viewEmployees = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS deparment, concat (e.first_name, " ", e.last_name) as Manager FROM employee JOIN role ON role.id = employee.role_id JOIN department ON role.department_id = department.id LEFT JOIN employee e ON e.id = employee.manager_id'
 
 const startQ = [
     {
@@ -57,7 +56,7 @@ const addRoleQ = [
         type: 'list',
         name: 'department',
         message: 'Select the department the new position belongs to',
-        choices: arrayDepartments //check this
+        choices: arrayDepartments
     }
 ]
 
@@ -76,95 +75,128 @@ const addEmployeeQ = [
         type: 'list',
         name: 'role',
         message: "What is the employee's role?",
-        choices: 'arrayRoles' //check this
+        choices: arrayRoles
     },
     {
         type: 'list',
         name: 'manager',
         message: "Enter employee's manager",
-        choices: 'arrayEmployees' //check this, need to add None to the options
+        choices: arrayEmployees
     },
 ]
 
+const viewRoles= 'SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON  role.department_id = department.id ORDER BY role.id'
+const viewEmployee = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS deparment, concat (e.first_name, " ", e.last_name) as Manager FROM employee JOIN role ON role.id = employee.role_id JOIN department ON role.department_id = department.id LEFT JOIN employee e ON e.id = employee.manager_id'
 
 function init() {
     inquirer
         .prompt(startQ)
         .then((response) => {
-            console.log(`\n Selected: ${response.action} \n`)
             
             switch (response.action) {
                 case 'View all employees':
-                    db.query(viewEmployees, (err, results) => 
-                        console.table(results))
-                    init()
+                    db.query(viewEmployee, (err, results) => {
+                        if (err) throw err
+                        console.table(results)
+                        init()
+                    })
                     break
                 case 'Add employee':
-                    console.log('Add employee')
+                    addEmployee()
                     break
                 case 'Update employee role':
                     console.log('Update employee role')
                     break
                 case 'View all roles':
-                    db.query(viewRoles, (err, results) =>
-                        console.table(results))
-                    // db.promise().query({sql: 'SELECT * FROM role', rowsAsArray: true})
-                    // .then(([rows, fields]) => {
-                    //     console.log(rows)
-                    // })
-                    init()
+                    db.query(viewRoles, (err, results) => {
+                        if (err) throw err
+                        console.table(results)
+                        init()
+                    })
                     break
                 case 'Add role':
                     addRole()
                     break
                 case 'View all departments':
-                    db.query('SELECT * FROM department', (err, results) =>
-                    console.table(results))
-                    init() 
+                    db.query('SELECT * FROM department', (err, results) => {
+                        if (err) throw err
+                        console.table(results)
+                        init()
+                    }) 
                     break                   
                 case 'Add department':
                     addDepartment()
                     break
                 default:           
                     console.log('Exit')
-                    break
             }
-
-        console.log(`\n`)
-        })    
+        // console.log(`\n`)
+    }) 
 }
 
 function addDepartment() {
     inquirer
         .prompt(addDepartmentQ)
         .then((response) => {
-            db.query(`INSERT INTO department (name) VALUES('${response.newDept}')`)
-            // db.promise().query(`INSERT INTO department (name) VALUES('${response.newDept}')`)
-                // .then(([rows, fields]) => {
-                    // console.table(rows)
-                    console.log (`\n New Department has beed added to the database \n`)
-                    init()
-                })
+            db.query(`INSERT INTO department (name) VALUES('${response.newDept}')`, (err, response) => {
+                if(err) throw err; 
+                console.log (`\n New Department has been added to the database \n`)
+                init() })
+        })
                         
 }
 
+function addEmployee() {
+    // Add all current roles in arrayRoles to insert in the inquirer list
+    db.query({sql: 'SELECT title FROM role', rowsAsArray: true}, function(err, results, fields) {
+        for (let i=0; i<results.length; i++) {
+            arrayRoles.push(results[i][0])
+        }
+    })
+    
+    // Add all current employees in arrayEmployees to insert in the inquirer list
+    db.query({sql: 'SELECT concat(first_name, " ", last_name) FROM employee', rowsAsArray: true}, function(err, results, fields) {
+        for (let i=0; i<results.length; i++) {
+            arrayEmployees.push(results[i][0])
+        }
+        arrayEmployees.push('Null')
+    })
+        
+    inquirer
+        .prompt(addEmployeeQ)
+        .then((response) => {
+            // db.query(`INSERT INTO role (title) VALUES('${response.title}')`)    
+            
+            console.log (`\n New employee has been added to the database \n`)
+            init()
+        })                   
+}
+
 function addRole() {
+    // Add current department names in arrayDepartments to insert in the inquirer list
     db.query({sql: 'SELECT name FROM department', rowsAsArray: true}, function(err, results, fields) {
         for (let i=0; i<results.length; i++) {
             arrayDepartments.push(results[i][0])
         }
-        console.log(arrayDepartments)
+        // console.log(arrayDepartments)
     })
-        
+
     inquirer
         .prompt(addRoleQ)
         .then((response) => {
-            // db.query(`INSERT INTO role (title) VALUES('${response.title}')`)    
-            
-            console.log (`\n New Department has beed added to the database \n`)
-            init()
-        })
-                        
+            let roleDept = ''
+                db.promise().query({sql: `SELECT id FROM department WHERE name = '${response.department}'`, rowsAsArray: true})
+                .then(([rows, fields]) => {
+                    // console.table(rows)
+                    roleDept = rows[0][0]
+                    // console.log('role:', roleDept)
+                    db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${response.title}', ${response.salary}, ${roleDept});`)
+                    // .then(() => init());
+                    init()
+                })
+       
+            console.log (`\n New role has been added to the database \n`)
+        })                   
 }
 
 init()
